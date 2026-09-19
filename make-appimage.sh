571c0dbc0a1ce466a5f4fb4843c486d2e60fed8f
ArqamQazi <arqamqazi549@gmail.com>
2026-09-19 21:54:53 +0530

    fix(make-appimage): set LD_LIBRARY_PATH and deploy ClassIn via quick-sharun

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,8 +9,11 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export DESKTOP=/usr/share/applications/classin.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/classin.svg
 
+# Allow ldd to resolve ClassIn's private libraries during deployment
+export LD_LIBRARY_PATH="/opt/apps/classin/lib:${LD_LIBRARY_PATH:-}"
+
 # Deploy dependencies
-quick-sharun /opt/apps/classin/ClassIn /opt/apps/classin/html/lib/* /opt/apps/classin/lib/* /usr/lib/libgtk-3.so*
+quick-sharun /opt/apps/classin/* /opt/apps/classin/html/lib/* /opt/apps/classin/lib/* /usr/lib/libgtk-3.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
